9ebae5a59952976b62e2e7db7d6fa55abbaa66a0
pralhad gautam <pralhadgautam@pralhads-MacBook-Air.local>
2025-12-17 21:45:14 +0545

    Prepare project for Render deployment

--- prarambha/settings.py
+++ prarambha/settings.py
@@ -26,6 +26,11 @@ SECRET_KEY = 'django-insecure-2$r76%574fda)e%u%&ot!7gr^8*e+bwd-snzt0gp#clh*_qimy
 DEBUG = True
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 
 
 # Application definition
@@ -49,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
